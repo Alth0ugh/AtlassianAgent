f942b07fd5fc04215e0cae9a95eeefd192d37097
Oliver <oliver.piter159@gmail.com>
2025-08-27 20:50:08 +0200

    Implement stdio Jira server

---
+++ servers/jira/jira_server_stdio.py
@@ -0,0 +1,211 @@
+from mcp.server.fastmcp import FastMCP
+import requests
+import base64
+from argparse import ArgumentParser
+from jira_create_request import CreateRequest
+from server_response import Response
+import json
+from dataclasses import asdict
+
+parser = ArgumentParser()
+parser.add_argument("--host", type=str, help="IP address of the server", default="0.0.0.0")
+parser.add_argument("--port", type=int, help="Port on which the server will be running.", default=8050)
+parser.add_argument("--space", type=str, help="Name of the Atlassian space.", required=True)
+parser.add_argument("--mail", type=str, help="Name of the Atlassian space.", required=True)
+parser.add_argument("--token", type=str, help="Name of the Atlassian space.", required=True)
+args = parser.parse_args()
+
+mcp = FastMCP(
+    name="JiraServer"
+)
+
+def convert_credentials(mail: str, token: str) -> str:
+    credentials = f"{mail}:{token}"
+    string_bytes = credentials.encode("utf-8")
+    base64_bytes = base64.b64encode(string_bytes)
+    return base64_bytes.decode("utf-8")
+
+def get_headers(credentials: str) -> dict:
+        return {"Authorization": f"Basic {credentials}",
+        "Accept": "application/json",
+        "Content-Type": "application/json"}
+
+def get_user_id(mail: str) -> str | None:
+    credentials = convert_credentials(args.mail, args.token)
+    headers = get_headers(credentials)
+    parameters = {
+        "query": f"{mail}"
+    }
+    response = requests.get(f"https://{args.space}.atlassian.net/rest/api/3/user/search", headers=headers, params=parameters)
+
+    if response.status_code == 200:
+        response = response.json()
+        assignee_id = response[0]["accountId"]
+        return assignee_id
+    return None
+
+@mcp.tool(description="""Creates a ticket in Jira. 
+          Parameters:
+          summary: summary of the ticket
+          description: description of the ticket
+          project: project where ticket is assigned
+          assignee: (Optional) assignee for the ticket. Provide user email.
+
+          Returns:
+          A response object with the following properties: is_error: boolean indicating whether an error occured, error_message: string containing error message if an error occured.""")
+def create_ticket(summary: str, description: str, project: str, assignee: str | None) -> str:
+    credentials = convert_credentials(args.mail, args.token)
+    headers = get_headers(credentials)
+    assignee_id = None
+
+    if assignee is not None:
+        parameters = {
+            "query": f"{assignee}"
+        }
+        response = requests.get(f"https://{args.space}.atlassian.net/rest/api/3/user/search", headers=headers, params=parameters)
+
+        if response.status_code == 200:
+            response = response.json()
+            assignee_id = response[0]["accountId"]
+
+
+    request = CreateRequest(project, summary, description, assignee_id)
+    response = requests.post(f"https://{args.space}.atlassian.net/rest/api/3/issue", data=request.to_json(), headers=headers)
+
+    if response.status_code == 201:
+        return json.dumps(asdict(Response(False)))
+    elif response.status_code == 400:
+        return json.dumps(asdict(Response(True, "The request was malformed.")))
+    elif response.status_code == 401:
+        return json.dumps(asdict(Response(True, "User cannot be authenticated.")))
+    elif response.status_code == 403:
+        return json.dumps(asdict(Response(True, "User does not have permissions for this operaiton.")))
+    elif response.status_code == 422:
+        return json.dumps(asdict(Response(True, "Configuration problem prevents execution of this operation.")))
+    return json.dumps(asdict(Response(True, "No valid credentials were found in the request.")))
+
+@mcp.tool(description="""Assigns user to an issue.
+          
+          Parameters:
+          mail: email of the user
+          issue_key: the key of the issue
+          
+          Returns:
+          A response object with the following properties: is_error: boolean indicating whether an error occured, error_message: string containing error message if an error occured.
+          """)
+def assign_user_to_issue(mail: str, issue_key: str) -> str:
+    user_id = get_user_id(mail)
+    credentials = convert_credentials(args.mail, args.token)
+    headers = get_headers(credentials)
+    body = {"accountId": user_id}
+
+    response = requests.put(f"https://{args.space}.atlassian.net/rest/api/3/issue/{issue_key}/assignee", headers=headers, data=json.dumps(body))
+
+    if response.status_code == 204:
+        return json.dumps(asdict(Response(False)))
+    elif response.status_code == 404:
+        return json.dumps(asdict(Response(True, "The user or the issue does not exist")))
+    
+    return json.dumps(asdict(Response(True, "Unexpected error occured")))
+
+@mcp.tool(description="""Gets all available status transitions for Jira issue.
+          
+          Parameters:
+          issue_key: key of the issue
+          
+          Returns:
+          A list of transition names and IDs.
+          In case of error returns a response object with the following properties: is_error: boolean indicating whether an error occured, error_message: string containing error message if an error occured.
+          """)
+def get_available_transitions(issue_key: str) -> str:
+    credentials = convert_credentials(args.mail, args.token)
+    headers = get_headers(credentials)
+
+    response = requests.get(f"https://{args.space}.atlassian.net/rest/api/3/issue/{issue_key}/transitions", headers=headers)
+    if response.status_code != 200:
+        return json.dumps(asdict(Response(True, "Could not load available transitions")))
+    
+    response = response.json()
+
+    transitions = response["transitions"]
+    transitions_list = []
+    for transition in transitions:
+        transitions_list.append({"name": transition["name"], "id": transition["id"]})
+    
+    return json.dumps(transitions_list)
+
+@mcp.tool(description="""Transitions Jira issue from one state to another.
+          
+          Parameters:
+          issue_key: key of the issue
+          transition_id: ID of the transition to be applied
+          
+          Returns:
+          A response object with the following properties: is_error: boolean indicating whether an error occured, error_message: string containing error message if an error occured.
+          """)
+def transition_issue(issue_key: str, transition_id: str) -> str:
+    credentials = convert_credentials(args.mail, args.token)
+    headers = get_headers(credentials)
+    body = {"transition": transition_id}
+
+    response = requests.post(f"https://{args.space}.atlassian.net/rest/api/3/issue/{issue_key}/transitions", headers=headers, data=json.dumps(body))
+    
+    if response.status_code == 204:
+        return json.dumps(asdict(Response(False)))
+    elif response.status_code == 400:
+        return json.dumps(asdict(Response(True, "This status does not exist.")))
+    elif response.status_code == 404:
+        return json.dumps(asdict(Response(True, "Issue does not exist.")))
+    return json.dumps(asdict(Response(True, "Unexpected error occured.")))
+
+@mcp.tool(description="""Lists all tickets assigned to a project. This tools does not need any parameters. 
+          
+          Returns:
+          A list of issues with key, summary and description of each issue. 
+          If error occurs, returns json with the following properties: is_error: boolean indicating whether an error occured, error_message: string containing error.""")
+def load_tickets_for_project(project_key: str) -> str:
+    credentials = convert_credentials(args.mail, args.token)
+    headers = get_headers(credentials)
+
+    params = {
+        "jql": f'project = "{project_key}" ORDER BY created DESC',
+              "fields": "summary, description,assignee,status"
+    }
+
+    response = requests.get(f"https://{args.space}.atlassian.net/rest/api/3/search/jql", params=params, headers=headers)
+
+    if response.status_code != 200:
+        return json.dumps(asdict(Response(True, "Issues could not be loaded.")))
+    
+    results = []
+    response = response.json()
+    issues = response.get("issues", [])
+    for issue in issues:
+        key = issue["key"]
+        summary = issue["fields"]["summary"]
+        status = issue["fields"]["status"]["name"]
+        assignee = issue["fields"]["assignee"]
+        assignee_name = ""
+        assignee_email = ""
+
+        if assignee is not None:
+            assignee_name = issue["fields"]["assignee"].get("displayName", "")
+            assignee_email = issue["fields"]["assignee"].get("emailAddress", "")
+
+        description_obj = issue["fields"].get("description")
+        description_text = ""
+        if description_obj and "content" in description_obj:
+            # Extract plain text from nested description
+            description_text = " ".join(
+                node["text"]
+                for block in description_obj["content"]
+                for node in block.get("content", [])
+                if node["type"] == "text"
+            )
+        results.append({"key": key, "summary": summary, "description": description_text, "status": status, "assignee_name": assignee_name, "assignee_mail": assignee_email})
+
+    return json.dumps(results)
+
+if __name__ == "__main__":
+    load_tickets_for_project("KAN")
+    mcp.run(transport="stdio")
